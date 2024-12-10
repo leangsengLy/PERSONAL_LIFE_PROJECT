663fe92c9ae2a162c80f87da437ee0adbea9867c
leangsengLy <edeepleang@gmail.com>
2024-12-11 06:59:24 +0700

    update new ui

--- Frontend/FRONTE_PRO/src/Page/Profile/ColorSystem.jsx
+++ Frontend/FRONTE_PRO/src/Page/Profile/ColorSystem.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
 
 function ColorSystem() {
   return (
-    <div>ColorSystem</div>
+    <div className='w-full'>
+        <h2>Theme</h2>
+        <p>In the below for switch the theme between mode white and dark</p>
+        <div className='wrapper-color flex gap-2 flex-wrap mt-3'>
+          {
+            [1,2,3,42,3,4,5,6,6,6,1,2,2,5,2,3,4,5,6,1,2,3,3,2,3,3,3,3,3,3,3,3,3,4,1,2,2].map((val)=>{
+              return (<><div className='w-[40px] cursor-pointer h-[40px] rounded-md bg-white'>
+                </div></>)
+            })
+          }
+        </div>
+        <div className='w-full flex h-auto justify-center items-center flex-col gap-y-5 mt-8'>
+            <p>Pick up your color sysyem</p>
+            <div className='w-[70px] h-[70px] rounded-full bg-red-500'></div>
+        </div>
+    </div>
   )
 }
 
--- Frontend/FRONTE_PRO/src/Page/Profile/Language.jsx
+++ Frontend/FRONTE_PRO/src/Page/Profile/Language.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 function Language() {
   return (
-    <div>Language</div>
+    <div className='w-full'>
+        <h2>Language</h2>
+        <p>In the below you can choose your language in our system </p>
+        <div className='wrapper-color flex gap-2 flex-wrap mt-3'>
+          {
+            [1,3,4,1,2,2].map((val)=>{
+              return (<><div className='w-[80px] h-[50px] rounded-md '>
+                <img className='w-full h-full object-cover  rounded-md ' src='https://upload.wikimedia.org/wikipedia/commons/8/83/Flag_of_Cambodia.svg'/>
+              </div></>)
+            })
+          }
+        </div>
+        <div className='w-full flex h-auto justify-center items-center flex-col gap-y-5 mt-8'>
+            <p>Pick up your color sysyem</p>
+            <div className='w-[130px] h-[80px] rounded-md '>
+              <img className='w-full h-full object-cover  rounded-md ' src='https://upload.wikimedia.org/wikipedia/commons/8/83/Flag_of_Cambodia.svg'/>
+            </div>
+        </div>
+    </div>
   )
 }
 
