6fe1458628a589b34733cd4013ada90e56eba3be
leangsengLy <edeepleang@gmail.com>
2025-06-15 23:08:10 +0700

    feat: refactor HomeMenu layout for improved structure and styling consistency

--- Frontend/FRONTE_PRO/src/Component/HomeMenu.jsx
+++ Frontend/FRONTE_PRO/src/Component/HomeMenu.jsx
@@ -39,28 +39,33 @@ const HomeMenu = () => {
   }
   return (
     <div className='w-screen h-screen relative px-10 lz-animation bg-box-wrapper flex justify-center items-center flex-wrap color-2'>
-        <div onClick={BackToBranch} className='absolute  flex   flex-col justify-center gap-y-5 items-center top-[160px] text-[30px] font-bold color-primary cursor-pointer'>
-            <div>V/Are-System </div>
-           <div className='px-4 py-2 border-slate opacity-70 transition-all ease-linear text-[15px] hover:opacity-100 rounded-xl'>
-            <i class="ri-database-2-fill color-primary text-[18px]"></i> {translateBy({en:currentBranch?.EnglishName,km:currentBranch?.Name})} </div>
-           </div>
+        
         <ActionTopRight isLoginOrisHomePage={true} />
-        <div className='max-w-[705px] lzscroll   max-h-[550px] flex flex-col justify-center items-center'>
-          <div className='wrapper-menu select-none  w-full h-full flex justify-center items-center flex-wrap gap-[21px]'>
-              {
-                drawSubMenu.map((val)=>{
-                  return (
-                    <>
-                    <Tooltip placement='top' showArrow={true} onClick={()=>{onClickMenu(val.code)}} content={translateBy({en:val.EnglishName,km:val.Name})}>
-                        <div  onClick={()=>{onClickMenu(val.code)}} className='w-[100px] cursor-pointer h-[100px] rounded-3xl border-primary flex justify-center items-center'>
-                          <i className={`${val.iconImage} text-[45px] color-primary`} />
-                      </div>
-                    </Tooltip>
-                  </>
-                  )
-                })
-              }
+        <div className='w-full'>
+          <div  className=' flex   flex-col justify-center gap-y-5 items-center text-[30px] font-bold color-primary cursor-pointer'>
+            <div>V/Are-System </div>
+            <div onClick={BackToBranch} className='px-4 py-2 border-slate mb-6 opacity-70 transition-all ease-linear text-[15px] hover:opacity-100 rounded-xl'>
+             <i class="ri-database-2-fill color-primary text-[18px]"></i> {translateBy({en:currentBranch?.EnglishName,km:currentBranch?.Name})} 
           </div>
+          <div className='max-w-[705px] lzscroll w-full  max-h-[550px] flex flex-col justify-center items-center'>
+            <div className='wrapper-menu select-none  w-full h-full flex justify-center items-center flex-wrap gap-[26px]'>
+                {
+                  drawSubMenu.map((val)=>{
+                    return (
+                      <>
+                      <Tooltip placement='top' showArrow={true} onClick={()=>{onClickMenu(val.code)}} content={translateBy({en:val.EnglishName,km:val.Name})}>
+                          <div  onClick={()=>{onClickMenu(val.code)}} className='w-[100px] cursor-pointer h-[100px] rounded-3xl border-primary flex justify-center items-center'>
+                            <i className={`${val.iconImage} text-[32px] color-primary`} />
+                        </div>
+                      </Tooltip>
+                    </>
+                    )
+                  })
+                }
+            </div>
+        </div>
+        </div>
+          
         </div>
     </div>
   )
